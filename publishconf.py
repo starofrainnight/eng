fc06cb45b9d16da7157a17dd804fbf39489fb599
Hong-She Liang <starofrainnight@gmail.com>
2016-05-09 22:28:32 +0800

    Fixed all links linked to "cht" not "eng"

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://starofrainnight.github.io/cht'
+SITEURL = 'https://starofrainnight.github.io/eng'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
